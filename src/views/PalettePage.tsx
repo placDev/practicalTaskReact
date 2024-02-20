05299d578cc6f1421e239566022fdce006c4c670
placDev <108148379+placDev@users.noreply.github.com>
2024-02-20 23:31:34 +0300

    [+] Начало работы с палитрой

--- src/views/PalettePage.tsx
+++ src/views/PalettePage.tsx
@@ -1,9 +1,21 @@
+import CSSModules from "react-css-modules";
+import styles from "../styles/palettePage.module.css"
+import { useRef } from "react";
 const PalettePage = () => {
+
+    const canvas = useRef<HTMLCanvasElement>(null);
+
     return (
         <>
-            <h1 className="font-weight-300">Палитра</h1>
+            <h1 className="font-weight-300">Палитра (и не только)</h1>
+            <section styleName="content">
+                <div styleName="palette"></div>
+                <div styleName="canvas">
+                    <canvas styleName="canvas-element" ref={canvas}>Очень жаль, обновите браузер</canvas>
+                </div>
+            </section>
         </>
     );
 }
 
-export default PalettePage;
+export default CSSModules(PalettePage, styles);
